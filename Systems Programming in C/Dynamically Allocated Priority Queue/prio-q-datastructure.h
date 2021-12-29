b9917505f0d79dec92a5c58551126fa903cca022
Liam Egan <81772560+liamegan21122@users.noreply.github.com>
2021-12-29 18:05:49 -0500

    Update prio-q-datastructure.h

--- Systems Programming in C/Dynamically Allocated Priority Queue/prio-q-datastructure.h
+++ Systems Programming in C/Dynamically Allocated Priority Queue/prio-q-datastructure.h
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /* (c) Liam Egan, 2020
- * UID: 115889972
  * This is the Prio_que struct that defines the fields
  * of a Prio_que variable. Note that the Prio_que
  * and node defintion share the same struct because
